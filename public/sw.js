5974545f61e59fb20c91ecb647d628eee5e40766
Thaenor <echo.elyst@gmail.com>
2020-05-10 23:19:54 +0100

    add service worker for offline func

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('airhorner').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/auth',
+                '/creator',
+                '/main',
+                '/demo',
+                '/caretaker',
+                '/404.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
